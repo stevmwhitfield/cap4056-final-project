ccf92fc3be3f6f57a8df23d26e82d2eda415cbdc
Steven Whitfield <stevmwhitfield@gmail.com>
2023-04-21 09:27:58 -0400

    Add credits to main menu; start setup for WAN

--- JumpRace/Assets/Scripts/MainMenu.cs
+++ JumpRace/Assets/Scripts/MainMenu.cs
@@ -2,31 +2,58 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-	#region FIELDS
-	[SerializeField] private Canvas mainCanvas;
-	[SerializeField] private Canvas settingsCanvas;
-	#endregion
+  #region FIELDS
 
-	#region M_MENU_BUTTON_CALLBACKS
-	public void OnStart() {
-		SceneManager.LoadScene(1);
-	}
+  [SerializeField] private Canvas mainCanvas;
+  [SerializeField] private Canvas settingsCanvas;
+  [SerializeField] private Canvas creditsCanvas;
+
+  #endregion
+
+  #region UNITY
+
+  void Start() {
+    string[] args = System.Environment.GetCommandLineArgs();
+    foreach (string arg in args) {
+      if (arg.StartsWith("PORT_") || arg.Contains("MASTER")) {
+        // Load WAN scene
+        SceneManager.LoadScene(2);
+      }
+    }
+  }
+
+  #endregion
+
+  #region MENU_BUTTON_CALLBACKS
+
+  public void OnStartLAN() {
+    SceneManager.LoadScene(1);
+  }
+
+  public void OnStartWAN() {
+    SceneManager.LoadScene(2);
+  }
 
   public void OnReturnToMainMenu() {
     SceneManager.LoadScene(0);
   }
 
-	public void OnReturn() {
+  public void OnReturn() {
     SetActiveMenu(0);
-	}
-  
-	public void OnSettings() {
-    SetActiveMenu(1);
-	}
+  }
 
-	public void OnQuit() {
-		Application.Quit(0);
-	}
+  public void OnSettings() {
+    SetActiveMenu(1);
+  }
+
+  public void OnCredits() {
+    SetActiveMenu(2);
+  }
+
+  public void OnQuit() {
+    Application.Quit(0);
+  }
+
   #endregion
 
   private void SetActiveMenu(int menu) {
@@ -34,10 +61,17 @@ public class MainMenu : MonoBehaviour {
       case 0:
         mainCanvas.enabled = true;
         settingsCanvas.enabled = false;
+        creditsCanvas.enabled = false;
         break;
       case 1:
         mainCanvas.enabled = false;
         settingsCanvas.enabled = true;
+        creditsCanvas.enabled = false;
+        break;
+      case 2:
+        mainCanvas.enabled = false;
+        settingsCanvas.enabled = false;
+        creditsCanvas.enabled = true;
         break;
     }
   }
